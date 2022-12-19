65625db8af83c8712ba09395cef91d1df32f4242
eimamovic2 <eimamovic2@etf.unsa.ba>
2022-12-19 19:14:08 +0100

    Zadnji dodaci za stranicu

--- client/src/Deck.tsx
+++ client/src/Deck.tsx
@@ -1,3 +1,66 @@
+import React, { useEffect, useState } from 'react'
+import { TDeck } from './api/getDecks';
+import './Deck.css'
+import { createCard } from './api/createCard';
+import { useParams } from "react-router-dom";
+import { getDeck } from './api/getDeck';
+import { deleteCard } from './api/deleteCard';
+
+
 export default function Deck() {
-    return <>Hello world</>;
+  const [deck, setDeck] = useState<TDeck | undefined> ();
+  const [cards, setCards] = useState<string[]>([]);
+  const [text, setText] = useState("");
+  let {deckId} = useParams();
+
+  async function handleCreateDeck(e: React.FormEvent){
+    e.preventDefault();
+    const { cards: serverCards} = await createCard(deckId!, text);
+    setCards(serverCards);
+    setText("");
+  }
+
+  useEffect(() => {
+    
+      async function fetchDeck(){
+      if(!deckId) return;
+      const newDeck = await getDeck(deckId);
+      setDeck(newDeck);
+      setCards(newDeck.cards);
+    }
+   fetchDeck();
+  }, [deckId]);
+
+  async function handleDeleteCard(index: number) {
+    if(!deckId) return;
+    const newDeck = await deleteCard(deckId, index);
+    setCards(newDeck.cards);
+    //setDecks(decks.filter(deck=> deck._id !== deckId));
+  }
+
+  return (
+    <div className="Deck">
+      <h1>{deck?.title}</h1>
+      <div className="cards">
+        {
+          cards.map((card, index) => (
+            <li key={index}>
+              <button onClick={() => handleDeleteCard(index)}>X</button>
+              {card}
+              </li>
+          ))}
+      </div>
+      <form onSubmit={handleCreateDeck}>
+        <label htmlFor="card-text">Card text</label>
+        <input id="card-text"
+          value={text}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setText(e.target.value);
+            }
+          }
+        />
+        <button>Create Card</button>
+      </form>
+    </div>
+  );
 }
---
+++ client/src/Header.tsx
@@ -0,0 +1,17 @@
+import './Header.css';
+import { Link } from "react-router-dom";
+export function Header(){
+    return <div className="Header">
+        <div className="container">
+            <div>
+                <a href="/">ToDo Cards</a> 
+            </div>
+            <div>
+                <a href="/">Decks</a> 
+            </div>
+            <div>
+                <a href="/login">login</a> 
+            </div>
+        </div>
+    </div>
+}
